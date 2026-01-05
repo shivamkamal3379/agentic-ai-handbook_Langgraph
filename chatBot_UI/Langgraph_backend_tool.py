973b688f8df5a99cec243bcb0f3e3c4aa9f42894
Shivam Kamal <shivamkamal3379@gmail.com>
2026-01-06 00:32:51 +0530

    tool calling with status updating on UI with tool status in containers

--- chatBot_UI/Langgraph_backend_tool.py
+++ chatBot_UI/Langgraph_backend_tool.py
@@ -3,7 +3,7 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
@@ -18,8 +18,13 @@ load_dotenv()
 # -------------------
 # 1. LLM
 # -------------------
-llm = ChatOpenAI()
-
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=1.0, 
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+) 
 # -------------------
 # 2. Tools
 # -------------------
