59ff539e5b6d75c40e672a83aedc29a6fc84caf0
Shivam Kamal <shivamkamal3379@gmail.com>
2026-01-23 17:58:01 +0530

    settled up .env secrets

--- Langsmith/1_Simple_LLM_Call.py
+++ Langsmith/1_Simple_LLM_Call.py
@@ -4,7 +4,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
-
+# simple LLM Call
 # Simple one-line prompt
 prompt = PromptTemplate.from_template("{question}")
 
@@ -12,14 +12,15 @@ model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=1.0, 
     max_tokens=None,
-    timeout=None,
+    timeout=None, 
     max_retries=2,
 ) 
-parser = StrOutputParser()
+parser = StrOutputParser() 
 
-# Chain: prompt → model → parser
-chain = prompt | model | parser
+# Chain: prompt → model → parser 
+chain = prompt | model | parser 
 
-# Run it
+# Run it 
 result = chain.invoke({"question": "What is the capital of Peru?"})
 print(result)
+ 
